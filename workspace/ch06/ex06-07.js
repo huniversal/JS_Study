04b943234462bd5f50b1f70e5549e4b0da0b8dbd
huniversal <hunivers0523@naver.com>
2025-04-28 14:05:55 +0900

    feat: 객체 인터렉션 타입

--- workspace/ch06/ex06-07.js
+++ workspace/ch06/ex06-07.js
@@ -1,4 +1,13 @@
 "use strict";
 // 타입 별칭으로 객체의 타입 선언
 (() => {
+    const u1 = {
+        name: "hunjin",
+        age: 20,
+    };
+    const u2 = {
+        name: "daun",
+        age: 30,
+    };
+    console.log(u1.age, u2.name.toUpperCase);
 })();
--- workspace/ch06/ex06-07.ts
+++ workspace/ch06/ex06-07.ts
@@ -1,5 +1,18 @@
 // 타입 별칭으로 객체의 타입 선언
 
-(()=>{
-  
+(() => {
+  // 타입의 별칭 생성
+  type User = {
+    name: string;
+    age: number;
+  };
+  const u1: User = {
+    name: "hunjin",
+    age: 20,
+  };
+  const u2: User = {
+    name: "daun",
+    age: 30,
+  };
+  console.log(u1.age, u2.name.toUpperCase);
 })();
