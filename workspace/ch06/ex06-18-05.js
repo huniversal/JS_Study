aeeac854fe9ee1cd149b8ecf4f739379216361fb
huniversal <hunivers0523@naver.com>
2025-04-29 14:36:16 +0900

    Feat: 타입 추론

--- workspace/ch06/ex06-18-05.js
+++ workspace/ch06/ex06-18-05.js
@@ -1,5 +1,34 @@
 "use strict";
 // 드롭다운 리스트 생성 - 제네릭 인터페이스와 함수
 // ex06-18-04.ts 복사
+// 드롭다운 리스트 생성 - 유니온 타입
+// ex06-18-03.ts 복사
+// 드롭다운 리스트 생성 - 타입스크립트
+// ex06-18-02.js 복사
+// 드롭다운 리스트 생성 - 자바스크립트
 (() => {
+    const cityList = [
+        { value: "Seoul", selected: false },
+        { value: "busan" },
+        { value: "GwangJu", selected: true },
+        // { value: 56789 },
+    ];
+    const zipcodeList = [
+        { value: 12345, selected: false },
+        { value: 34567, selected: true },
+        { value: 56789 },
+    ];
+    // FIXME: 아래 출력 결과와 같이 출력 되도록 함수 작성
+    // function createDropdownList(list: DropdownItem<string | number>[]) {
+    function createDropdownList(list) {
+        let value;
+        let options = "";
+        for (let item of list) {
+            value = item.value;
+            options += `\t<option selected="${item.selected ? "selected" : ""}">${typeof value === "string" ? value.toUpperCase() : value}</option>\n`;
+        }
+        return `<select>\n${options}</select>`;
+    }
+    console.log(createDropdownList(cityList));
+    console.log(createDropdownList(zipcodeList));
 })();
--- workspace/ch06/ex06-19.ts
+++ workspace/ch06/ex06-19.ts
@@ -1,5 +1,14 @@
 // 변수의 타입 추론
 
-(()=>{
-  
+(() => {
+  // 기본 데이터 타입의 변수 타입을 추론에 의지(권장)
+  let name = "이훈진"; // string으로 타입 추론
+
+  const age = 20; // number로 타입 추론
+
+  let name2; // any로 타입 추론
+  name2 = "이훈진";
+  name2 = 219;
+
+  console.log(name, age, name2);
 })();
