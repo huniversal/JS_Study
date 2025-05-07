8a2df1ac6f314102da57340bac2a72ee28f49da6
huniversal <hunivers0523@naver.com>
2025-05-07 15:40:16 +0900

    promise 및 동기, 비동기

---
+++ workspace/ch08/ex08-02.ts
@@ -0,0 +1,19 @@
+// 에러 핸들링 - 에러가 발생하는 경우
+// ex08-01.ts 복사
+
+(() => {
+  function f1() {
+    //const obj = null as any;
+    const obj = undefined;
+    //obj.fn(); // TypeError 발생
+
+    const fn = new Function("x", "y", "retun x + y"); // return 오타. SyntaxError 발생
+    console.log(fn(10, 20));
+  }
+
+  function f2() {
+    f1();
+  }
+
+  f2();
+})();
