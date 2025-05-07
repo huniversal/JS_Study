c5bde629113e084167aa21b7c0eb5ab10dc9f5cd
huniversal <hunivers0523@naver.com>
2025-05-07 10:27:25 +0900

    실습 세팅

--- workspace/ch07/ex07-16.js
+++ workspace/ch07/ex07-16.js
@@ -4,10 +4,19 @@
     const arr = [10, 20, 30];
     // FIXME arr의 각 요소를 제곱하여 새로운 배열을 생성하는 코드를 작성하세요.(for문)
     let newArr = [];
-    console.log('for문', newArr);
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i] ** 2);
+    }
+    console.log("for문", newArr);
     // FIXME arr의 각 요소를 제곱하여 새로운 배열을 생성하는 코드를 작성하세요.(forEach)
     newArr = [];
-    console.log('forEach', newArr); // [100, 400, 900]
+    arr.forEach((e) => {
+        newArr.push(e ** 2);
+    });
+    console.log("forEach", newArr); // [100, 400, 900]
     // FIXME arr의 각 요소를 제곱하여 새로운 배열을 생성하는 코드를 작성하세요.(map)
-    console.log('map', newArr); // [100, 400, 900]
+    arr.map((e) => {
+        return e ** 2;
+    });
+    console.log("map", newArr); // [100, 400, 900]
 })();
