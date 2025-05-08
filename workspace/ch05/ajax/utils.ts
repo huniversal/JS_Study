9a28261cc5ba3aa7a2cf057625c334b5bdc56197
huniversal <hunivers0523@naver.com>
2025-05-08 13:15:57 +0900

    feat: 이미지 전송

--- workspace/ch05/ajax/utils.ts
+++ workspace/ch05/ajax/utils.ts
@@ -1,5 +1,14 @@
 import { Cat } from "./types.js";
 
-export function appendImages(images: Cat[]){
-  
+export function appendImages(images: Cat[]) {
+  const catList = document.querySelector("#cat-list");
+
+  images.forEach((items) => {
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    img.src = items.url;
+    img.height = 200;
+    li.appendChild(img);
+    catList?.appendChild(li);
+  });
 }
