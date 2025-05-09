f07633835c0e58428a3d27b2bc1b0657ef9408d6
huniversal <hunivers0523@naver.com>
2025-05-09 10:38:09 +0900

    feat: 실시간 채팅 구현

--- workspace/ch05/ajax/ex05-13.js
+++ workspace/ch05/ajax/ex05-13.js
@@ -7,16 +7,21 @@ btn?.addEventListener("click", getImages);
 async function getImages() {
     try {
         const response = await fetch(url);
+        // fetch()로 API 요청 보냄
+        // await는 기다렸다가 응답 받는다.
         console.log(response);
         if (response.ok) {
+            // response.ok는 HTTP 요청이 성공적으로 처리됐는지 확인하는 값
             const data = await response.json();
             appendImages(data);
+            // 요청에 성공했으면 .json()으로 데이터 꺼냄
+            // appendImages(data)로 화면에 추가
         }
         else {
             console.log(response.status, "에러 발생");
-        }
+        } // 실패하면 상태코드 찍고 에러 메시지 출력
     }
     catch (err) {
         console.error("에러 발생", err);
-    }
+    } // fetch() 자체가 실패할 경우
 }
